abecde4ea0c84e40ff7b4a641c600c850871fd95
gongzhi <gongzhi@corp.netease.com>
2026-04-06 12:58:27 +0800

    fix(lint): sort imports and remove unreachable code to pass CI checks

--- src/renderer/components/Toast.tsx
+++ src/renderer/components/Toast.tsx
@@ -1,5 +1,5 @@
+import { CheckCircleIcon,InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import React from 'react';
-import { XMarkIcon, InformationCircleIcon, CheckCircleIcon } from '@heroicons/react/24/outline';
 
 interface ToastProps {
   message: string;
